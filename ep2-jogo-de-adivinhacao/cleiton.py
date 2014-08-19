ed7224efa6551c9718c9401bcc1462be2408287f
Cleiton Melo <cleiton.melo@gmail.com>
2014-08-19 18:25:15 -0300

    Adicionado o padrao PEP8

--- ep2-jogo-de-adivinhacao/cleiton.py
+++ ep2-jogo-de-adivinhacao/cleiton.py
@@ -2,21 +2,21 @@
 # -*- encoding: utf-8 -*-
 
 import random
-random.seed() # inicializa o contador
-s = random.randint(1,100) #gera numero randomico inteiro
+random.seed()   # inicializa o contador
+s = random.randint(1, 100)   # gera numero randomico inteiro
 n = 0
 t = 0
 while True:
-	try:
-		n = int(raw_input("Digite o numero de 1 a 100: " ))
-		t = t + 1
-		if n == s:
-			print "Resultado CORRETO, valor sorteado %d" % s
-			print "Parabens voce acertou o resultado em %d tentativas" % t
-			break
-		elif s > n :
-			print "Sorteado numero MAIOR, tente novamente"
-		elif s < n :
-			print "Sorteado numero MENOR, tente novamente"
-	except ValueError:
-		print "Favor digitar um numero inteiro valido de 0 a 100."
+    try:
+        n = int(raw_input("Digite o numero de 1 a 100: "))
+        t = t + 1
+        if n == s:
+            print "Resultado CORRETO, valor sorteado %d" % s
+            print "Parabens voce acertou o resultado em %d tentativas" % t
+            break
+        elif s > n:
+            print "Sorteado numero MAIOR, tente novamente"
+        elif s < n:
+            print "Sorteado numero MENOR, tente novamente"
+    except ValueError:
+        print "Favor digitar um numero inteiro valido de 1 a 100."
